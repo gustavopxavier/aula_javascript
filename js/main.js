0c39094a7b82f921f772307f734fa98bc4790203
gustavopxavier <gustavopxavier@gmail.com>
2021-03-02 07:28:23 -0300

    Aula 1 - Introduçao ao Javascript

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Gustavo Xavier";
+var idade = 40;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert("Bem vindo " + nome + " e você tem " + idade + " anos ");
+// alert(idade + idade2)
+console.log(nome)
+console.log(idade + idade2)
+console.log(idade * idade2)
+console.log(frase.toUpperCase())
+console.log(frase.toLocaleLowerCase())
+// alert(frase.replace("Japão", "Brasil"))
