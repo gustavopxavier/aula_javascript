932e35a05be67b0f2f2fccfa045e60078ff54515
gustavopxavier <gustavopxavier@gmail.com>
2021-03-02 08:31:17 -0300

    Aula 2 - Introduçao ao Javascript

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
-var nome = "Gustavo Xavier";
-var idade = 40;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo";
-//alert("Bem vindo " + nome + " e você tem " + idade + " anos ");
-// alert(idade + idade2)
-console.log(nome)
-console.log(idade + idade2)
-console.log(idade * idade2)
-console.log(frase.toUpperCase())
-console.log(frase.toLocaleLowerCase())
-// alert(frase.replace("Japão", "Brasil"))
+var frutas = [{nome: "Maçã", cor:"Vermelha"}, {nome: "Uva", cor:"Verde"}]
+console.log(frutas)
+alert(frutas[1].nome)
+// alert(frutas.cor)
+
+// var fruta = {nome: "Maçã", cor:"Vermelha"}
+// console.log(fruta.nome)
+// alert(fruta.cor)
+// var lista = ["Maçã", "Pêra", "Laranja"]
+// lista.push("Uva")
+// // lista.pop()
+
+
+// console.log(lista)
+// console.log(lista.toString())
+// console.log(lista.join(" / "))
+
+// var nome = "Gustavo Xavier";
+// var idade = 40;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time do mundo";
+// //alert("Bem vindo " + nome + " e você tem " + idade + " anos ");
+// // alert(idade + idade2)
+// console.log(nome)
+// console.log(idade + idade2)
+// console.log(idade * idade2)
+// console.log(frase.toUpperCase())
+// console.log(frase.toLocaleLowerCase())
+// // alert(frase.replace("Japão", "Brasil"))
