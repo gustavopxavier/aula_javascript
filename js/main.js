705bde28b44148392d7216bf9350598d7f26f56e
gustavopxavier <gustavopxavier@gmail.com>
2021-03-02 12:20:15 -0300

    Parte 2 - Aula 1 - Introduçao ao Javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-var d = new Date()
-alert(d.getDay())
-alert(d.getHours())
-alert(d.getMinutes())
+function soma(n1, n2){
+    return n1 + n2
+}
 
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18)
+        validar = true
+    else
+        validar = false
+    return validar
+}
+
+var idade = prompt("Qual sua idade?")
+console.log(validaIdade(idade))
+
+// alert (soma(5,10))
+
+// var d = new Date()
+// alert(d.getDay())
+// alert(d.getHours())
+// alert(d.getMinutes())
 
 // var count
 // for(count=0; count <=5; count++) {
