67d121a7b50a8bceff2f0cba2d77783015d22204
gustavopxavier <gustavopxavier@gmail.com>
2021-03-02 18:13:16 -0300

    Parte 2 - Aula 2 - Introduçao ao Javascript

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
-function soma(n1, n2){
-    return n1 + n2
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>"
+    // console.log(document.getElementById("agradecimento"))
+    // alert("Obrigado por clicar!")
 }
 
-function validaIdade(idade){
-    var validar;
-    if(idade >= 18)
-        validar = true
-    else
-        validar = false
-    return validar
+function redirecionar(){
+    // window.open("https://www.google.com.br")
+    window.location.href = "https://www.google.com.br"
 }
 
-var idade = prompt("Qual sua idade?")
-console.log(validaIdade(idade))
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse"
+    // document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    // alert("Trocar texto")
+}
+
+function voltar(elemento){
+    // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load(){
+    alert("Página carregada!")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+// function soma(n1, n2){
+//     return n1 + n2
+// }
+
+// function validaIdade(idade){
+//     var validar;
+//     if(idade >= 18)
+//         validar = true
+//     else
+//         validar = false
+//     return validar
+// }
+
+// var idade = prompt("Qual sua idade?")
+// console.log(validaIdade(idade))
 
 // alert (soma(5,10))
 
