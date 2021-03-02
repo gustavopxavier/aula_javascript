f6a5f1ac7abd77eabc66258f227f826d60638e03
gustavopxavier <gustavopxavier@gmail.com>
2021-03-02 09:29:54 -0300

    Aula 2 - Introduçao ao Javascript

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-var frutas = [{nome: "Maçã", cor:"Vermelha"}, {nome: "Uva", cor:"Verde"}]
-console.log(frutas)
-alert(frutas[1].nome)
+var d = new Date()
+alert(d.getDay())
+alert(d.getHours())
+alert(d.getMinutes())
+
+// var count
+// for(count=0; count <=5; count++) {
+//     alert(count)
+// }
+
+// var count = 0;
+// while (count < 10) {
+//     console.log(count)
+//     alert(count)
+//     count++
+// }
+// var idade = prompt("Qual a sua idade?")
+// if (idade >= 18)
+//     alert("Maior de idade")
+// else 
+//     alert("Menor de idade")
+
+// var frutas = [{nome: "Maçã", cor:"Vermelha"}, {nome: "Uva", cor:"Verde"}]
+// console.log(frutas)
+// alert(frutas[1].nome)
 // alert(frutas.cor)
 
 // var fruta = {nome: "Maçã", cor:"Vermelha"}
